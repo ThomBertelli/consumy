52b5a2bf4d08b11ffba0971273ce430c797140a6
Thomas Breno Bertelli Mendes <bertellimendes@gmail.com>
2024-03-07 17:49:59 -0300

    first commit

---
+++ src/components/FirstComponent.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+    const message = defineModel('message',{default: "Hello World"})
+</script>
+
+<template>
+    <div>
+
+        <h1>Meu primeiro componente</h1> 
+    
+        <div>Dizendo olá, para variar: {{ message }}</div>
+
+        <input v-model="message">
+
+    </div>
+
+</template>
