b6e25b31ab15f889a4a2e3b533115cbc1b342149
Thomas Breno Bertelli Mendes <bertellimendes@gmail.com>
2024-04-18 09:24:08 -0300

    atualiza o SignIn para adicionar a funcionalidade 'lembrar'

--- src/components/SignIn.vue
+++ src/components/SignIn.vue
@@ -1,13 +1,14 @@
 <script lang="ts" setup>
 import { useRouter } from 'vue-router'
 import { ref } from 'vue'
-import { auth } from '../auth'
+import { Auth } from '../auth'
 import Swal from 'sweetalert2'
 
 
 const router = useRouter()
-const email = defineModel('email')
-const password = defineModel('password')
+const email = defineModel<string>('email')
+const password = defineModel<string>('password')
+const remember = defineModel<boolean>('remember', { default: true })
 const awaiting = ref(false)
 const visibility = ref("password")
 
@@ -16,9 +17,10 @@ const togglePasswordVisibility = () => {
     visibility.value = visibility.value === 'password' ? 'text' : 'password';
 }
 
-function onSubmit() {
+function onSubmit(form: Event) {
+    let auth = new Auth(remember.value)
     awaiting.value = true
-    auth.signIn(email.value as string, password.value as string, () => {
+    auth.signIn(email.value || '', password.value || '', () => {
         awaiting.value = false
         Swal.fire({
             icon: "success",
@@ -28,9 +30,9 @@ function onSubmit() {
             showConfirmButton: false,
             timer: 2000,
             timerProgressBar: true,
-            width: 200,    
-        }).then(() => {router.push('/') }) 
-        
+            width: 200,
+        }).then(() => { router.push('/') })
+
     }, () => {
         awaiting.value = false
         Swal.fire({
@@ -42,8 +44,8 @@ function onSubmit() {
             timer: 3000,
             timerProgressBar: true,
             width: 250,
-            
-                
+
+
         })
     })
 }
@@ -51,7 +53,7 @@ function onSubmit() {
 
 <template>
     <div class=" backimage flex flex-col justify-center items-center h-screen w-screen">
-        <form @submit.prevent="onSubmit()" class=" form-layout max-w-md mx-auto flex flex-col items-center">
+        <form @submit.prevent="onSubmit" class=" form-layout max-w-md mx-auto flex flex-col items-center">
             <div class="mb-4">
                 <label for="email" class="block text-pink-700 text-lg font-bold mb-3">Email</label>
                 <input v-model="email" type="email" id="email" name="email"
@@ -60,7 +62,7 @@ function onSubmit() {
             </div>
             <div class="mb-4 relative">
                 <label for="password" class="block text-pink-700 text-lg font-bold mb-3">Senha</label>
-                
+
                 <div class=" flex items-center">
                     <input v-model="password" :type="visibility" id="password" name="password"
                         class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
@@ -69,8 +71,21 @@ function onSubmit() {
                         <font-awesome-icon :icon="visibility === 'password' ? ['fas', 'eye'] : ['fas', 'eye-slash']" />
                     </a>
                 </div>
-                
+
+                <div class="pt-4">
+                    <label class="inline-flex items-center cursor-pointer">
+                        <input type="checkbox" value="" class="sr-only peer" v-model="remember">
+                        <div
+                            class="relative w-11 h-6 bg-gray-200 rounded-full peer peer-focus:ring-4 peer-focus:ring-pink-300 dark:peer-focus:ring-pink-800 dark:bg-gray-900 peer-checked:after:translate-x-full rtl:peer-checked:after:-translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-0.5 after:start-[2px] after:bg-white after:border-black-400 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-pink-600">
+                        </div>
+                        <span class="ms-3 text-sm font-medium text-gray-900 dark:text-gray-300">Lembrar-se</span>
+                    </label>
+                </div>
+
             </div>
+
+
+
             <div v-show="!awaiting" class="flex items-center justify-between">
                 <button type="submit"
                     class="font-bold text-white cursor-pointer bg-pink-700 hover:bg-pink-600 px-5 py-2 rounded-lg text-lg hover:scale-105">
@@ -85,15 +100,14 @@ function onSubmit() {
 
 
 <style scoped>
-    .backimage{
-        background-image: url('../assets/images/background_signin.png');
-        background-size: cover;
-    }
+.backimage {
+    background-image: url('../assets/images/background_signin.png');
+    background-size: cover;
+}
 
-    .form-layout{
-        background-color:rgb(5, 5, 5);
-        padding: 50px 100px;
-        border-radius: 25px;
-    }
-
+.form-layout {
+    background-color: rgb(5, 5, 5);
+    padding: 50px 100px;
+    border-radius: 25px;
+}
 </style>
