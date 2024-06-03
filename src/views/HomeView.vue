02166f0daaf786d281080da9a41a1412c45f5a21
Thomas Breno Bertelli Mendes <bertellimendes@gmail.com>
2024-06-03 18:59:51 -0300

    build: ajust dependencies

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -3,7 +3,7 @@ import HeaderComponent from '../components/HeaderComponent.vue';
 import BannerComponent from '../components/BannerComponent.vue'
 
 
-</script>s not listed within the file list of project
+</script>
 
 <template>
     <div>
