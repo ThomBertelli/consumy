29b2394cb88dacfcb81d7918dff64f320bf85219
Thomas Breno Bertelli Mendes <bertellimendes@gmail.com>
2024-06-05 20:46:58 -0300

    feat: add StoresContainer component

--- src/components/BannerComponent.vue
+++ src/components/BannerComponent.vue
@@ -5,12 +5,12 @@ import { RouterLink, RouterView } from 'vue-router'
 
 
 <template>
-    <div class=" font-bold banner flex justify-center items-center h-screen w-screen bg-cover">
+    <div class=" font-bold banner flex justify-center items-center w-screen bg-cover">
 
         <div class="text-center banner-text">
-            <h1 class=" mx-auto text-4xl mt-5">Cadastre-se!</h1>
+            <h1 class=" mx-auto text-4xl mt-2">Crie sua conta e desfrute dos melhores restaurantes da sua região, do conforto da sua casa.</h1>
             <RouterLink to="/cadastrar">
-                <ButtonPrime type="button">Cadastrar</ButtonPrime>
+                <ButtonPrime class="text-black font-bold" type="button">Criar Conta</ButtonPrime>
             </RouterLink>
         </div>
     </div>
@@ -19,12 +19,12 @@ import { RouterLink, RouterView } from 'vue-router'
 
 <style scoped>
     .banner{
-        
+        padding: 50px;
         background-image: url('../assets/images/fundo-home.jpg');
     }
     .banner-text{
         background-color:rgba(255, 255, 255, 0.753);
-        padding: 100px;
+        padding: 40px;
         border-radius: 25px;
     }
 
---
+++ src/components/StoresContainer.vue
@@ -0,0 +1,21 @@
+<script lang="ts" setup>
+    import { ref, onMounted } from 'vue';
+    import StoreCard from './StoreCard.vue';
+    const stores = ref([]);
+
+    onMounted(async () => {
+    const response = await fetch('URL_DA_SUA_API');
+    const data = await response.json();
+    stores.value = data;
+    });
+</script>
+
+<template>
+    <div>
+        <StoreCard v-for="store in stores" :key="store.id" :store="store" />
+    </div>
+</template>
+
+<style lang="css" scoped>
+    
+</style>
