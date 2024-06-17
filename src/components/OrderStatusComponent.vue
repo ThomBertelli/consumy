f8565a7a58ecfed85324859df57ca255ade03904
Thomas Breno Bertelli Mendes <bertellimendes@gmail.com>
2024-06-16 21:07:20 -0300

    feat:(CheckoutComponent) ajust component to watch order state

--- src/components/OrderStatusComponent.vue
+++ src/components/OrderStatusComponent.vue
@@ -5,39 +5,85 @@ import { RouterLink } from 'vue-router'
 const apiCredential = import.meta.env.VITE_API_CREDENTIAL
 const apiUrl = import.meta.env.VITE_API_URL;
 const orderState = ref()
+import { fetchEventSource } from '@microsoft/fetch-event-source';
 
 const props = defineProps({
     orderId: String
 });
 
-const fetchOrder = async () => {
-    try {
-        const response = await fetch(`${apiUrl}/orders/${props.orderId}`, {
-            headers: {
-                "Accept": "application/json",
-                "Content-Type": "application/json",
-                "X-API-KEY": `${apiCredential}`,
-                "Authorization": `Bearer ${localStorage.getItem("token")}`
+// const fetchOrder = async () => {
+//     try {
+//         const response = await fetch(`${apiUrl}/orders/${props.orderId}`, {
+//             headers: {
+//                 "Accept": "application/json",
+//                 "Content-Type": "application/json",
+//                 "X-API-KEY": `${apiCredential}`,
+//                 "Authorization": `Bearer ${localStorage.getItem("token")}`
+//             }
+//         }
+
+//         )
+//         if (!response.ok) {
+//             throw new Error('Erro ao buscar pedido')
+//         }
+//         const data = await response.json()
+//         orderState.value = data.state
+
+//     } catch (error) {
+//         console.error('Erro ao buscar pedido:', error)
+//     }
+// }
+
+let controller;
+
+const openSSE = async () => {
+    console.log('vai')
+    let id = props.orderId
+    let url = `${apiUrl}/orders/${id}/watch`;
+
+    controller = new AbortController();
+    const signal = controller.signal;
+
+    fetchEventSource(url, {
+        method: 'GET',
+        headers: {
+            'Accept': 'application/json',
+            'Authorization': `Bearer ${localStorage.getItem('token')}`,
+            'X-API-KEY': apiCredential,
+        },
+        signal: signal,
+        async onopen(response) {
+            if (response.ok) {
+                console.log('connected!');
+                return; // everything's good
+            }
+        },
+        onmessage(msg) {
+            if (msg.event === 'order-paid') {
+                orderState.value = 'paid'
+                
+            } 
+            if (msg.event === 'order-not-paid') {
+                orderState.value = 'notPaid'
+                
             }
         }
 
-        )
-        if (!response.ok) {
-            throw new Error('Erro ao buscar pedido')
-        }
-        const data = await response.json()
-        orderState.value = data.state
 
-    } catch (error) {
-        console.error('Erro ao buscar pedido:', error)
-    }
+    });
+
 }
+
+
+
 
 console.log(orderState)
 
 onMounted(() => {
-    fetchOrder()
+    openSSE()
 });
+
+
 
 
 
@@ -55,15 +101,17 @@ onMounted(() => {
         <div v-if="orderState == 'notPaid'" class="flex flex-col gap-2 items-center">
             <i class="pi pi-times-circle text-8xl text-red-500"></i>
             <p>Falha no pagamento!</p>
+            
         </div>
-
+        
+        <p v-if="orderState != 'paid' && orderState != 'notPaid'" >Estamos processando seu pagamento...</p>
+        
 
         <RouterLink :to="{ name: 'stores' }">
             <ButtonPrime class="text-black font-bold">
                 Continuar Comprando
             </ButtonPrime>
         </RouterLink>
-
 
 
     </div>
