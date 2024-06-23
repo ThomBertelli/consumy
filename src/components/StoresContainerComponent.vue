2bbd53e3d807dab320998eb714e67468645a63fd
Thomas Breno Bertelli Mendes <bertellimendes@gmail.com>
2024-06-23 20:39:52 -0300

    feat(StoreContainet): add show more button to deal with pagination

--- src/components/StoresContainerComponent.vue
+++ src/components/StoresContainerComponent.vue
@@ -1,16 +1,19 @@
 <script lang="ts" setup>
-    import { ref, onMounted } from 'vue';
-    import StoreCard from './StoreCardComponent.vue';
+import { ref, onMounted } from 'vue';
+import StoreCard from './StoreCardComponent.vue';
 
-    const apiCredential = import.meta.env.VITE_API_CREDENTIAL
-    const apiUrl = import.meta.env.VITE_API_URL;
-    const stores = ref([]);
+const apiCredential = import.meta.env.VITE_API_CREDENTIAL
+const apiUrl = import.meta.env.VITE_API_URL;
+const stores = ref([]);
+const totalPages = ref()
+const pageNumber = ref(1)
 
-    onMounted( async () => {
+const fetchStores = async () => {
     try {
-        const response = await fetch(`${apiUrl}/stores`, {
+        const response = await fetch(`${apiUrl}/stores?page=${pageNumber.value}`, {
             headers: {
                 'Accept': 'application/json',
+                "X-API-KEY": `${apiCredential}`,
                 "Authorization": `Bearer ${localStorage.getItem("token")}`
             }
         }
@@ -21,21 +24,36 @@
         }
         const data = await response.json()
         console.log(data)
-        stores.value = data
+        for (let store of data.result.stores) {
+            if (!stores.value.includes(store)) {
+                stores.value.push(store)
+            }
+
+        }
+
 
     } catch (error) {
         console.error('Erro ao buscar lojas:', error)
     }
 }
-    )
+
+const showMore = () =>{
+    pageNumber.value++
+    fetchStores()
+}
+
+
+onMounted(() => { fetchStores() })
 </script>
 
 <template>
     <div class=" pt-40 px-10 items-center w-screen grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 xl:grid-cols-4 gap-4">
         <StoreCard v-for="store in stores" :key="store.id" :store="store" />
+        <div v-if="pageNumber < totalPages" class="mt-10" >
+            <ButtonPrime outlined @click="showMore">Mostrar mais</ButtonPrime>
+        </div>
     </div>
+
 </template>
 
-<style lang="css" scoped>
-    
-</style>
+<style lang="css" scoped></style>
