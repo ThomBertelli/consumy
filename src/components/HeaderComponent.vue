bf7db8c4d7bc46522c19bab8a5cb0817fca4aaea
Thomas Breno Bertelli Mendes <bertellimendes@gmail.com>
2024-06-23 21:31:12 -0300

    feat(StoreProducts): add show more button to deal with pagination

--- src/components/HeaderComponent.vue
+++ src/components/HeaderComponent.vue
@@ -26,7 +26,7 @@ const signOut = () => {
 
 <template>
 
-    <div class=" fixed bg-black text-white flex justify-between w-full p-5 items-center">
+    <div class=" absolute bg-black text-white flex justify-between w-full p-5 items-center">
         <div class="flex items-center gap-10">
             <RouterLink :to="{name: 'home'}">
                 <img class=" cursor-pointer h-20 w-auto rounded-3xl" src="../assets/images/delivery-logo.png" alt="">
--- src/components/StoreProductsContainer.vue
+++ src/components/StoreProductsContainer.vue
@@ -11,11 +11,13 @@ const route = useRoute();
 const apiCredential = import.meta.env.VITE_API_CREDENTIAL
 const apiUrl = import.meta.env.VITE_API_URL;
 const cartStore = useCartStore();
+const totalPages = ref()
+const pageNumber = ref(1)
+const storeId = route.params.storeId;
 
-
-const fetchStore = async (id: string) => {
+const fetchStore = async () => {
     try {
-        const response = await fetch(`${apiUrl}/stores/${id}`, {
+        const response = await fetch(`${apiUrl}/stores/${storeId}`, {
             headers: {
                 "Accept": "application/json",
                 "Content-Type": "application/json",
@@ -36,41 +38,52 @@ const fetchStore = async (id: string) => {
     }
 }
 
-const fetchProducts = async (id: string) => {
+const fetchProducts = async () => {
+
     try {
-        const response = await fetch(`${apiUrl}/stores/${id}/products?locale=pt-BR`, {
+        const response = await fetch(`${apiUrl}/stores/${storeId}/products?page=${pageNumber.value}&locale=pt-BR`, {
             headers: {
-                "Accept": "application/json",
-                "Content-Type": "application/json",
+                'Accept': 'application/json',
+                "Authorization": `Bearer ${localStorage.getItem("token")}`,
                 "X-API-KEY": `${apiCredential}`,
-                "Authorization": `Bearer ${localStorage.getItem("token")}`
-            }
+
+            },
         }
 
         )
         if (!response.ok) {
-            throw new Error('Erro ao buscar produtos da loja')
+
+            throw new Error('Erro ao buscar produtos')
         }
         const data = await response.json()
-        products.value = data.result.products
+        totalPages.value = data.result.pagination.pages
+        for(let product of data.result.products){
+            console.log(product)
+            if (!products.value.includes(product)){
+                products.value.push(product)
+            }
+        }
 
     } catch (error) {
-        console.error('Erro ao buscar produtos da loja:', error)
+        console.error('Erro ao buscar produtos:', error)
     }
 }
 
-const setStoreInfo = (id) => {
-  cartStore.setStoreInfo(id);
+const setStoreInfo = () => {
+  cartStore.setStoreInfo(storeId);
 };
 
 onMounted(() => {
-    const storeId = route.params.storeId;
-    fetchStore(storeId)
-    fetchProducts(storeId)
-    setStoreInfo(storeId)   
+    
+    fetchStore()
+    fetchProducts()
+    setStoreInfo()   
 });
 
-
+const showMore = () =>{
+    pageNumber.value++
+    fetchProducts()
+}
 
 </script>
 
@@ -82,9 +95,11 @@ onMounted(() => {
         </div>
 
         <div class=" mt-10 px-10 items-center w-screen grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4">
-            <ProductCard v-for="product in products" :key="product.id" :product="product" />
+            <ProductCard class="z-1" v-for="product in products" :key="product.id" :product="product" />
         </div>
-
+        <div v-if="pageNumber < totalPages" class="my-10" >
+            <ButtonPrime outlined @click="showMore">Mostrar mais</ButtonPrime>
+        </div>
     </div>
 
 </template>
